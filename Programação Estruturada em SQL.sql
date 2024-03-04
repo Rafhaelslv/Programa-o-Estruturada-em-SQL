b39f2a894d049f40805ed64ea9b1f0100f0cf0dc
Rafhael Silva de Brito <127260453+Rafhaelslv@users.noreply.github.com>
2024-03-04 17:56:46 -0300

    Add files via upload

--- Programação Estruturada em SQL.sql
+++ Programação Estruturada em SQL.sql
@@ -1,4 +1,4 @@
---EXERC 01
+
 --Dado um número inteiro. Calcule e mostre o seu fatorial. (Não usar entrada superior a 12)
 DECLARE @num1 INT, @FAT INT, @AUX INT
 SET @num1 = 5
@@ -13,17 +13,38 @@ SET @AUX = @AUX - 1
 END
 PRINT @FAT
 
--- EXERC 04 
+--Dados A, B, e C de uma equação do 2o grau da fórmula AX2+BX+C=0. Verifique e mostre a
+--existência de raízes reais e se caso exista, calcule e mostre. Caso não existam, exibir mensagem.
+
+
+
+
+--Calcule e mostre quantos anos serão necessários para que Ana seja maior que Maria sabendo
+--que Ana tem 1,10 m e cresce 3 cm ao ano e Maria tem 1,5 m e cresce 2 cm ao ano.
+DECLARE @ana real , @maria real, @ano INT
+SET @ana = 1.10
+SET @maria  = 1.50
+SET @ano = 0
+
+WHILE (@ana < @maria)
+BEGIN
+SET @ana = @ana + 0.03
+SET @maria = @maria + 0.02
+SET @ano = @ano + 1
+END
+PRINT @ano
+
+
 --Seja a seguinte série: 1, 4, 4, 2, 5, 5, 3, 6, 6, 4, 7, 7, ...
 
 DECLARE @num INT, @cont INT, @numx INT
 SET @num = 1
 SET @numx = 4
-SET @cont = 0
+SET @cont = 1
 
 WHILE(@cont < 10)
 BEGIN
-	IF (@cont % 3 = 0)
+	IF (@cont % 2 = 1)
 	BEGIN
 		PRINT @num
 		SET @num = @num + 1
@@ -38,3 +59,23 @@ BEGIN
 	END
 
 END
+
+--Considerando a tabela abaixo, gere uma massa de dados, com 50 registros, para fins de teste
+--com as regras estabelecidas (Não usar constraints na criação da tabela)
+
+CREATE TABLE PRODUTO(
+codigo				int				NOT NULL IDENTITY(50001, 1),
+nome				varchar(30)		NOT NULL,
+valor				decimal(7,2)	NOT NULL,
+vencimento			date			NOT NULL
+)
+
+DECLARE @cod_produto		INT,
+		@nome_produto		VARCHAR(30),
+		@valor_produto		decimal(7,2),
+		@vencimento_produto date,
+		@id_produto			INT
+SET @id_produto = 1
+SET @id_produto = (SELECT @cod_produto FROM PRODUTO WHERE nome = @id_produto)
+PRINT @cod_produto
+PRINT @nome_produto
